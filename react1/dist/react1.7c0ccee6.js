865d4272b0d4f6466283ed533be853b729194db1
Rohit <rohitkokate1163@gmail.com>
2025-05-18 12:40:07 +0530

    run react app

---
+++ react1/dist/react1.7c0ccee6.js
@@ -0,0 +1,17 @@
+const heading = React.createElement("h1", {
+    className: "title"
+}, "Heading 1");
+const heading1 = React.createElement("h2", {
+    className: "title"
+}, "Heading 2");
+const container = React.createElement("div", {
+    id: "container"
+}, [
+    heading,
+    heading1
+]);
+// console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
+
+//# sourceMappingURL=react1.7c0ccee6.js.map
